89c304ff43ef4722975bbd5c0c860a2fe32fe7c6
nic-gunter-gtchs <nicolas.gunter@student.gtchs.org>
2025-08-10 10:28:39 -0400

    HEAVY optimizations

--- src/code/main.py
+++ src/code/main.py
@@ -1,74 +1,52 @@
 import pygame as pg
 import sys
-from time import sleep
+from collections import deque
 print("py_bytefall by Nic Gunter")
 source = input("Input file (including full path): ")
 delay = float(input("Delay between lines of rain (ms, higher values = slower): "))
 bsize = int(input("What size should the grid be? "))
 with open(source, "rb") as s:
-    data = s.read()
-    hbytes = data.hex()
-hold = []
+    hbytes = s.read().hex() # unnecessary variable removed, conversion now handled in one line
 bytelist = []
-for i in range(len(hbytes)):
-    if i % 2 == 0:
-        hold.append(hbytes[i])
-    else:
-        bytelist.append(''.join([hold[0],hbytes[i]]))
-        hold.clear()
-print(bytelist) # debug
-r = 0 # red value
-g = 0 # green value
-b = 0 # blue value
-colref = {}
-for i in range(256):
-    colref[i] = (r,g,b)
-    r += 1
-    g += 1
-    b += 1
+for i in range(0, len(hbytes), 2): # I don't know what the heck was going on here before, this is much faster and more readable
+    pair = hbytes[i:i+2] # slicing internally runs in C, makes a large improvement
+    bytelist.append(pair)
 colbytes = []
-for i in bytelist:
-    colbytes.append(colref[int(i,16)])
-print(colbytes) # debug
+for hexstr in bytelist:
+    val = int(hexstr, 16) #why did I use 3 vars for this?? idk, also who knows why I made a LUT for 0-255 :sob: now it just directly converts the hexbyte to decimal
+    colbytes.append((val, val, val)) # it's grayscale, only one value needed
 pg.init()
-def rain():
-    global x, y, tuplist, rectlist
-    for hi in range(len(colbytes)):
-        if x == GRID_SIZE:
-            y += bsize
-            x = 0
-            pg.display.flip()
-            sleep(delay/1000)
-        if y >= SIZE:
-            rec = len(rectlist)
-            screen.fill((255, 255, 255))
-            for re in range(rec):
-                tuplist = rectlist[0]
-                if tuplist[1] == 0:
-                    del rectlist[0]
-                else:
-                    pg.draw.rect(screen, tuplist[2], (tuplist[0], tuplist[1] - bsize, bsize, bsize))
-                    rectlist.append([tuplist[0], tuplist[1] - bsize, tuplist[2]])
-                    del rectlist[0]
-            y -= bsize
-        pg.draw.rect(screen, colbytes[hi], (x * bsize, y, bsize, bsize))
-        rectlist.append([x * bsize, y, colbytes[hi]])
-        x += 1
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                pg.quit()
-                sys.exit()
-rectlist =[]
-tuplist = []
 SIZE = 640
-y = 0
-x = 0
 GRID_SIZE = SIZE // bsize
 screen = pg.display.set_mode((SIZE, SIZE))
-screen.fill((255,255,255))
-bytelist.clear()
-colref.clear()
-rain()
+screen.fill((255, 255, 255))
+pg.display.set_caption("py_bytefall")
+x, y = 0, 0
+rectlist = deque() # alright chatgpt helped me with this one, this data struct is more efficient for value access
+for color in colbytes:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
+            sys.exit()
+    if x == GRID_SIZE:
+        x = 0
+        y += bsize
+        pg.display.flip()
+        pg.time.delay(int(delay)) # pygame has its own delay builtin, no need for sleep, also prevents window handling bugs
+    if y >= SIZE:
+        screen.fill((255, 255, 255))
+        new_rectlist = deque()
+        for rx, ry, rc in rectlist:
+            if ry > 0:
+                ny = ry - bsize
+                pg.draw.rect(screen, rc, (rx, ny, bsize, bsize))
+                new_rectlist.append((rx, ny, rc))
+        rectlist = new_rectlist
+        y -= bsize
+    px = x * bsize
+    pg.draw.rect(screen, color, (px, y, bsize, bsize))
+    rectlist.append((px, y, color))
+    x += 1
 input("Done. Press ENTER to exit.")
 pg.quit()
 sys.exit()
